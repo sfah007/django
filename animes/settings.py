9d60114bd26314ed0fbd43bc6ee296c6cc90546f
hamza <hamzalahyane54@gmail.com>
2021-10-15 02:14:18 -0700

    first commit

--- animes/settings.py
+++ animes/settings.py
@@ -89,7 +89,7 @@ WSGI_APPLICATION = 'animes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -107,8 +107,7 @@ DATABASES = {
         'PASSWORD': 'admin'
     }
 }
-
-
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators

7fa91dc589ad10c2d62244395595a81470589a51
hamza <hamzalahyane54@gmail.com>
2021-10-15 01:41:46 -0700

    first commit

--- animes/settings.py
+++ animes/settings.py
@@ -89,7 +89,7 @@ WSGI_APPLICATION = 'animes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -100,14 +100,14 @@ DATABASES = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'animes',
+        'NAME': 'anime',
         'USER': 'postgres',
         'PORT': '5432',
         'HOST': 'localhost',
         'PASSWORD': 'admin'
     }
 }
-"""
+
 
 
 # Password validation

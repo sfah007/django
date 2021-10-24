ab09ce64f22b8bab8a12a35acb7f9960b718640b
hamza <hamzalahyane54@gmail.com>
2021-10-24 08:43:25 -0700

    first commit

--- animes/settings.py
+++ animes/settings.py
@@ -165,8 +165,8 @@ LOGIN_REDIRECT_URL = 'index'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'hamzalahyane54@gmail.com'
-EMAIL_HOST_PASSWORD = 'imanelahyane'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
